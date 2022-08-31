60aded104ec5e63ab090aa759be19bb4dd1462c8
berwyn <berwyn.hoyt@gmail.com>
2022-08-31 15:11:05 +1200

    Factor out snprintf into outputf for significantly prettier code

--- mlua.c
+++ mlua.c
@@ -18,10 +18,33 @@
 // we'd need to return this handle to the user instead of making it a global.
 lua_State *Global_lua = NULL;
 
+
+// like printf but fills gtm_string_t with up to maximum size
+// do nothing and return -2 if output is NULL or output->address is NULL
+// returns -1 if it had to truncate the output
+// otherwise return 0
+int outputf(gtm_string_t *output, int output_size, const char *fmt, ...) {
+  if (!output || !output->address) return -2;
+  va_list argp;
+  va_start(argp, fmt);
+  output->length = vsnprintf(output->address, output_size, fmt, argp);
+  va_end(argp);
+  if (output->length > output_size) {  // snprintf truncates but still returns full output size
+    output->length = output_size-1;   // drop final null
+    return -1;
+  }
+  return 0;
+}
+
 // Wrap luaL_openlibs to change it to type lua_CFunction so we can call it with protected pcall
 static int luaL_openlibs_ret0(lua_State *L) {
   luaL_openlibs(L);
   return 0; // return no parameters
+}
+
+// Return version numbers for this module
+gtm_int_t mlua_version_number(int _argc) {
+  return MLUA_VERSION_NUMBER;
 }
 
 // Create new Lua_State, and initialize with default lua libs
@@ -33,17 +56,13 @@ static int luaL_openlibs_ret0(lua_State *L) {
 gtm_long_t mlua_open(int argc, gtm_string_t *output, gtm_int_t flags) {
   lua_State *L;
   int output_size = output->length; // ydb sets it to preallocated size
-  if (argc<1 || !output->address) output=NULL; // don't return error string
+  if (argc<1) output=NULL; // don't return error string
   if (argc<2) flags=0;
 
   // allocate new lua state
   L = luaL_newstate();
   if (!L) {
-    if (output) {
-      output->length = snprintf(output->address, output_size, "Could not allocate lua_State -- possible memory lack");
-      if (output->length > output_size)  // snprintf truncates but still returns full output size
-        output->length = output_size-1; // drop final null
-    }
+    outputf(output, output_size, "MLua: Could not allocate lua_State -- possible memory lack");
     return 0;
   }
 
@@ -52,11 +71,7 @@ gtm_long_t mlua_open(int argc, gtm_string_t *output, gtm_int_t flags) {
   int args=0, results=0, error_handler=0;
   int error = lua_pcall(L, args, results, error_handler);
   if (error) {
-    if (output) {
-      output->length = snprintf(output->address, output_size, "Lua: in init luaL_openlibs(), %s", lua_tostring(L, -1));
-      if (output->length > output_size)  // snprintf truncates but still returns full output size
-        output->length = output_size-1; // drop final null
-    }
+    outputf(output, output_size, "Lua: in init luaL_openlibs(), %s", lua_tostring(L, -1));
     lua_pop(L, 1);  // pop error message from the stack
     return 0;
   }
@@ -73,20 +88,14 @@ gtm_long_t mlua_open(int argc, gtm_string_t *output, gtm_int_t flags) {
       error = luaL_loadbuffer(L, mlua_init, strlen(mlua_init), mlua_init);
     error |= lua_pcall(L, args, results, error_handler);
     if (error) {
-      if (output) {
-        output->length = snprintf(output->address, output_size, "Lua: MLUA_INIT, %s", lua_tostring(L, -1));
-        if (output->length > output_size)  // snprintf truncates but still returns full output size
-          output->length = output_size-1;  // drop final null
-      }
+      outputf(output, output_size, "Lua: MLUA_INIT, %s", lua_tostring(L, -1));
       lua_pop(L, 1);  // pop error message from the stack
       return 0;
     }
   }
 
-  if (output) {
-    output->address[0] = '\0';   // clear error string
-    output->length = 0;
-  }
+  // clear error string & return handle
+  outputf(output, output_size, "");
   return (gtm_long_t)L;
 }
 
@@ -111,7 +120,7 @@ void mlua_close(int argc, gtm_long_t luaState_handle) {
 gtm_int_t mlua_lua(int argc, const gtm_string_t *code, gtm_string_t *output, gtm_long_t luaState_handle, ...) {
   int output_size = output->length; // ydb sets it to preallocated size
   if (argc<1) return MLUA_ERROR;  // no code to run so return error status -- but can't return output string (not supplied)
-  if (argc<2 || !output->address) output=NULL; // don't return error string
+  if (argc<2) output=NULL; // don't return error string
   if (argc<3) luaState_handle=0; // use global lua_State
 
   // open global lua state if necessary
@@ -143,28 +152,18 @@ gtm_int_t mlua_lua(int argc, const gtm_string_t *code, gtm_string_t *output, gtm
     error |= lua_pcall(L, args, results, error_handler);
   }
   if (error) {
-    if (output) {
-      output->length = snprintf(output->address, output_size, "Lua: %s", lua_tostring(L, -1));
-      if (output->length > output_size)  // snprintf truncates but still returns full output size
-        output->length = output_size-1;  // drop final null
-    }
+    outputf(output, output_size, "Lua: %s", lua_tostring(L, -1));
     lua_pop(L, 1);  // pop error message from the stack
     return MLUA_ERROR;
   }
   if (output) {
-    output->length = snprintf(output->address, output_size, "%s", lua_tostring(L, -1));
-    if (output->length > output_size)  // snprintf truncates but still returns full output size
-      output->length = output_size-1;  // drop final null
-  }
-  if (lua_isnil(L, -1)) {
-    output->address[0] = '\0';
-    output->length=0;
+    // Test for nil first to speed up lua() invokations when it's not necessary to call slow outputf
+    if (lua_isnil(L, -1) && output->address) {
+      *output->address = '\0';
+      output->length = 0;
+    } else
+      outputf(output, output_size, "%s", lua_tostring(L, -1));
   }
   lua_pop(L, 1);  // pop result from the stack
   return 0;
-}
-
-// Return version numbers for this module
-gtm_int_t mlua_version_number(int _argc) {
-  return MLUA_VERSION_NUMBER;
 }
